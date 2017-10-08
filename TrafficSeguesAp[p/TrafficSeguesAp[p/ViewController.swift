452c81cccbb56f138037c375298fe9ee5474b469
Qasim Ahmed <qahme033@uottawa.ca>
2017-10-08 07:55:58 -0400

    save

--- TrafficSeguesAp[p/TrafficSeguesAp[p/ViewController.swift
+++ TrafficSeguesAp[p/TrafficSeguesAp[p/ViewController.swift
@@ -14,6 +14,8 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
+    @IBOutlet weak var textField: UITextField!
+    
     @IBAction func unwindToRed(unwindSegue : UIStoryboardSegue){
         
     }
@@ -23,6 +25,8 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
-
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        segue.destination.title = textField.text;
+    }
 }
 
