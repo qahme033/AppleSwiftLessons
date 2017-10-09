07466fecb8fb65f968a439969d852b1447c07629
Qasim Ahmed <qahme033@uottawa.ca>
2017-10-09 08:45:55 -0400

    finished segue assignment

--- TrafficSeguesAp[p/TrafficSeguesAp[p/ViewController.swift
+++ TrafficSeguesAp[p/TrafficSeguesAp[p/ViewController.swift
@@ -14,19 +14,23 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
-    @IBOutlet weak var textField: UITextField!
-    
-    @IBAction func unwindToRed(unwindSegue : UIStoryboardSegue){
-        
-    }
 
+    @IBOutlet weak var segueSwitch: UISwitch!
+    @IBAction func greenButtonTapped(_ sender: Any) {
+        if segueSwitch.isOn {
+            performSegue(withIdentifier: "Green", sender: nil)
+        }
+    }
+    @IBAction func yellowButtonTapped(_ sender: Any) {
+        if segueSwitch.isOn {
+            performSegue(withIdentifier: "Yellow", sender: nil)
+        }
+    }
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
 
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        segue.destination.title = textField.text;
-    }
 }
 
