35f6b6524f8478e8a265f0e43f4ffd733e63df39
Qasim Ahmed <qahme033@uottawa.ca>
2017-10-11 07:41:02 -0400

    done login ex

--- LoginQ/LoginQ/ViewController.swift
+++ LoginQ/LoginQ/ViewController.swift
@@ -10,14 +10,39 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var userNameTextField: UITextField!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
 
+
+    @IBAction func forgottenUsernamePressed(_ sender: Any) {
+        performSegue(withIdentifier: "ForgottenUsernameOrPassword", sender: sender)
+    }
+    
+    @IBAction func forgottenPasswordPressed(_ sender: Any) {
+        performSegue(withIdentifier: "ForgottenUsernameOrPassword", sender: sender)
+
+    }
+    @IBOutlet weak var forgottenPasswordButton: UIButton!
+    
+    @IBOutlet weak var forgottenUsernameButton: UIButton!
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
+    }
+
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        guard let sender = sender as? UIButton else { return };
+        if sender == forgottenUsernameButton{
+            segue.destination.navigationItem.title = "Forgotten Username"
+        }else if sender == forgottenPasswordButton {
+            segue.destination.navigationItem.title = "Forgotten Password"
+        }else {
+            segue.destination.navigationItem.title = userNameTextField.text;
+
+        }
     }
 
 
